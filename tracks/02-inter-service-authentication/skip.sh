5359e88e818de609c2bd27c9342585ef7290d62b
pseudonator <kasun.talwatta@gmail.com>
2022-12-13 23:54:54 +1300

    Added steps to deploy sample apps
    Signed-off-by: pseudonator <kasun.talwatta@gmail.com>

--- tracks/02-inter-service-authentication/skip.sh
+++ tracks/02-inter-service-authentication/skip.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+kubectl --context $WEST_CONTEXT create ns httpbin-trusted
+istioctl --context $WEST_CONTEXT kube-inject -r $ISTIO_REVISION -f $DIR/../../install/apps/httpbin/httpbin.yaml | kubectl --context $WEST_CONTEXT apply -n httpbin-trusted -f -
+
+kubectl --context $WEST_CONTEXT create ns http-client-trusted
+istioctl --context $WEST_CONTEXT kube-inject -r $ISTIO_REVISION -f $DIR/../../install/apps/http-client/http-client.yaml | kubectl --context $WEST_CONTEXT apply -n http-client-trusted -f -
+
+sleep 5
